928c00e2b35c50aa7d1beae76c691681cb591716
CCZX <ccdebuging@gmail.com>
2021-01-05 21:15:40 +0800

    feat: tsdx config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,10 +1,21 @@
-const scss = require('rollup-plugin-scss');
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
   rollup(config, options) {
-    config.plugins = config.plugins.concat([
-      scss(), // will output compiled styles to output.css
-    ]);
+    config.plugins = [
+      postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            preset: 'default',
+          }),
+        ],
+        extract: 'dist.min.css',
+      }),
+      ...config.plugins
+    ]
     return config;
   },
 };
